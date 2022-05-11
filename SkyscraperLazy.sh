1a7c42a281ed030a7728709ecbc05571cedd2f7f
SoumyBhow <soumybhow1.0@gmail.com>
2022-05-11 19:50:20 -0400

    Sort platforms

--- SkyscraperLazy.sh
+++ SkyscraperLazy.sh
@@ -1,38 +1,38 @@
 SYSTEMS=$(whiptail --title "Lazy Skyline ROM Scraping Script" --checklist "Please select the systems you want to scrape" 20 78 15 \
-    "atari2600" "Atari - 2600" OFF \
+	"3do" "The 3DO Company - 3DO" OFF \
+	"3ds" "Nintendo - Nintendo 3DS" OFF \
+	"arcade" "Arcade Games" OFF \
+	"atari2600" "Atari - 2600" OFF \
 	"atarilynx" "Atari - Lynx" OFF \
 	"doom" "DOOM" OFF \
 	"dos" "DOS" OFF \
-	"arcade" "Arcade Games" OFF \
-	"pcengine" "NEC - PC Engine - TurboGrafx 16" OFF \
-	"pcenginecd" "NEC - PC Engine CD - TurboGrafx-CD" OFF \
-	"gb" "Nintendo - Game Boy" OFF \
-	"gba" "Nintendo - Game Boy Advance" OFF \
-	"gbc" "Nintendo - Game Boy Color" OFF \
-	"gc" "Nintendo - GameCube" OFF \
-	"3ds" "Nintendo - Nintendo 3DS" OFF \
-	"n64" "Nintendo - Nintendo 64" OFF \
-	"nds" "Nintendo - Nintendo DS" OFF \
-	"nes" "Nintendo - Nintendo Entertainment System" OFF \
-	"pokemini" "Nintendo - Pokemon Mini" OFF \
-	"snes" "Nintendo - Super Nintendo Entertainment System" OFF \
-	"wii" "Nintendo - Wii" OFF \
-	"neogeo" "SNK - Neo Geo" OFF \
-	"neogeocd" "SNK - Neo Geo CD" OFF \
-	"ngp" "SNK - Neo Geo Pocket" OFF \
-	"ngpc" "SNK - Neo Geo Pocket Color" OFF \
-	"scummvm" "ScummVM" OFF \
-	"sega32x" "Sega - 32X" OFF \
 	"dreamcast" "Sega - Dreamcast" OFF \
 	"gamegear" "Sega - Game Gear" OFF \
-	"mastersystem" "Sega - Master System" OFF \
+	"gba" "Nintendo - Game Boy Advance" OFF \
+	"gbc" "Nintendo - Game Boy Color" OFF \
+	"gb" "Nintendo - Game Boy" OFF \
+	"gc" "Nintendo - GameCube" OFF \
 	"genesis" "Sega - Mega Drive - Genesis" OFF \
-	"segacd" "Sega - Mega-CD - Sega CD" OFF \
-	"saturn" "Sega - Saturn" OFF \
-	"psx" "Sony - PlayStation" OFF \
+	"mastersystem" "Sega - Master System" OFF \
+	"n64" "Nintendo - Nintendo 64" OFF \
+	"nds" "Nintendo - Nintendo DS" OFF \
+	"neogeocd" "SNK - Neo Geo CD" OFF \
+	"neogeo" "SNK - Neo Geo" OFF \
+	"nes" "Nintendo - Nintendo Entertainment System" OFF \
+	"ngpc" "SNK - Neo Geo Pocket Color" OFF \
+	"ngp" "SNK - Neo Geo Pocket" OFF \
+	"pcenginecd" "NEC - PC Engine CD - TurboGrafx-CD" OFF \
+	"pcengine" "NEC - PC Engine - TurboGrafx 16" OFF \
+	"pokemini" "Nintendo - Pokemon Mini" OFF \
 	"ps2" "Sony - PlayStation 2" OFF \
 	"psp" "Sony - PlayStation Portable" OFF \
-	"3do" "The 3DO Company - 3DO" OFF \
+	"psx" "Sony - PlayStation" OFF \
+	"saturn" "Sega - Saturn" OFF \
+	"scummvm" "ScummVM" OFF \
+	"sega32x" "Sega - 32X" OFF \
+	"segacd" "Sega - Mega-CD - Sega CD" OFF \
+	"snes" "Nintendo - Super Nintendo Entertainment System" OFF \
+	"wii" "Nintendo - Wii" OFF \
  3>&1 1>&2 2>&3)
 
 SOURCES=$(whiptail --title "Lazy Skyline ROM Scraping Script" --checklist "Please select the sources you want to fetch from" 3 78 15 \
